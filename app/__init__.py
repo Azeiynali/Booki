63867ae9133192eb9390857addd7e37b519a9246
Azeiynali <Azeiynali@gmail.com>
2023-12-26 16:01:08 +0330

    limit rate added

--- app/__init__.py
+++ app/__init__.py
@@ -2,20 +2,23 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 from flask_login import LoginManager
-
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../base.db'
-app.config['SECRET_KEY'] = os.urandom(23)
-app.config['UPLOAD_FOLDER'] = 'app/static/pictures'
-app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif', 'mp4'}
+limiter = Limiter(get_remote_address, app=app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///../base.db"
+app.config["SECRET_KEY"] = os.urandom(23)
+app.config["UPLOAD_FOLDER"] = "app/static/pictures"
+app.config["ALLOWED_EXTENSIONS"] = {"png", "jpg", "jpeg"}
 app.secret_key = os.urandom(24)
 
 db = SQLAlchemy(app)
 Login_mg = LoginManager(app)
-Login_mg.login_view = '/'
-Login_mg.login_message = ''
+Login_mg.login_view = "/"
+Login_mg.login_message = ""
 
 from app.routes import *
 
