b73a12ed97341339dba53439e858080bfc27b999
Azeiynali <Azeiynali@gmail.com>
2023-12-29 08:21:32 +0330

    logging and Bcrypt added

--- app/__init__.py
+++ app/__init__.py
@@ -4,16 +4,34 @@ import os
 from flask_login import LoginManager
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+import secrets
+import logging
+from flask_bcrypt import Bcrypt
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
-limiter = Limiter(get_remote_address, app=app)
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+)
+bcrypt = Bcrypt(app)
+app.logger = logging.getLogger(__name__)
+
+file_handler = logging.FileHandler("app.log")
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+app.logger.addHandler(file_handler)
+app.logger.setLevel(logging.DEBUG)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///../base.db"
 app.config["SECRET_KEY"] = os.urandom(23)
 app.config["UPLOAD_FOLDER"] = "app/static/pictures"
 app.config["ALLOWED_EXTENSIONS"] = {"png", "jpg", "jpeg"}
-app.secret_key = os.urandom(24)
+app.config["ALLOWED_EXTENSIONS"] = {"png", "jpg", "jpeg"}
+app.config["SECRET_KEY"] = secrets.token_hex(16)
+
+app.secret_key = secrets.token_hex(16)
 
 db = SQLAlchemy(app)
 Login_mg = LoginManager(app)
